aa153a8e24a94a7442273358cbe82a50cdd094b0
Sharun <sharun@sharunkumar.com>
2024-09-05 20:15:52 -0700

    comment out git changes for now

--- .github/workflows/auto-update.sh
+++ .github/workflows/auto-update.sh
@@ -11,9 +11,9 @@ latest_version=${latest_version#v}
 # Update the pkgver in PKGBUILD
 sed -i "s/^pkgver=.*/pkgver=$latest_version/" PKGBUILD
 
-# Commit and push the changes
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
-git config --global user.name "GitHub Actions"
-git add PKGBUILD
-git commit -m "chore: update flyctl to $latest_version"
-git push
+# # Commit and push the changes
+# git config --global user.email "github-actions[bot]@users.noreply.github.com"
+# git config --global user.name "GitHub Actions"
+# git add PKGBUILD
+# git commit -m "chore: update flyctl to $latest_version"
+# git push
