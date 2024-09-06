7e8ed6c6b611a3579b337b9f18efb0894b6eb869
Sharun <715417+sharunkumar@users.noreply.github.com>
2024-09-05 22:35:15 -0500

    Setup Auto-Update Workflow (#9)

---
+++ .github/workflows/auto-update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Get the latest release version from GitHub API
+latest_version=$(curl -s https://api.github.com/repos/superfly/flyctl/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+
+# Remove the 'v' prefix if present
+latest_version=${latest_version#v}
+
+# Update the pkgver in PKGBUILD
+sed -i "s/^pkgver=.*/pkgver=\"$latest_version\"/" PKGBUILD
+
+updpkgsums
+
+makepkg --printsrcinfo >.SRCINFO
+
+if [[ -n $(git status -s) ]]; then
+    # Ensure build passes
+    makepkg --noconfirm -si
+
+    git config --global user.email "sharun@sharunkumar.com"
+    git config --global user.name "Sharun"
+    git add PKGBUILD
+    git commit -m "chore: update to v$latest_version"
+    git push
+else
+    echo "No changes to commit."
+fi
